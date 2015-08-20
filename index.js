506c88f16274f06f6b7c67c2b4ec9e2e17764aaf
Mordy Tikotzky <mordytk@gmail.com>
2015-08-20 11:48:35 -0400

    add some react specific overrides

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ module.exports = {
 	plugins: ['filenames'],
 	rules: {
 		indent: [2, 'tab'],
+		// Enforce that filenames are camelcase
 		'filenames/filenames': 2,
+		// Prevent missing displayName in a React component definition
+		'react/display-name': 2,
+		// Prevent duplicate props in JSX
+		'react/jsx-no-duplicate-props': 2,
+		// Prevent missing props validation in a React component definition
+		'react/prop-types': 2,
 	},
 };
